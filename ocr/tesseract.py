1e5557b2f5940ec5c6491eb314db022d4d9643c3
mumei115 <creeper.daisuki@gmail.com>
2024-06-19 13:30:07 +0900

    ocr2

--- ocr/tesseract.py
+++ ocr/tesseract.py
@@ -6,7 +6,14 @@ import pyocr
 import pyocr.builders
 from PIL import Image, ImageEnhance, ImageFilter
 
-file_path = r".pdf"
+#JP Singapore Thailand Hanoi Indonesia UP MJIIT 
+
+inputfile = 
+
+outputfile = inputfile + "gray"
+file_path = r"" + inputfile + r".pdf"
+output_pdf_path = r"" + outputfile + r".pdf"
+
 
 gray_pdf = []
 
@@ -27,7 +34,7 @@ print("Will use lang '%s'" % (lang))
 all_text = []
 
 #pdfを画像に変換
-images = pdf2image.convert_from_path(file_path, dpi=350, fmt='png')
+images = pdf2image.convert_from_path(file_path, dpi=350, fmt='png' , first_page=3)
 lang = 'eng'
 #lang = 'jpn'
 #画像オブジェクトからテキストに変換
@@ -52,10 +59,11 @@ for image in images:
     )
     all_text.append(txt)
 
+
+gray_pdf[0].save(output_pdf_path, save_all=True, append_images=gray_pdf[1:])  
+
 full_text = "\n".join(all_text)
 print(full_text)
-
-
 
 ocr_list = []
 
@@ -77,6 +85,12 @@ for match in matches:
 
 print(len(ocr_list))
 
+#すべての数値でOCR出来たのか確認す
+def check_len_list(lis):
+    if len(lis) % 9 == 0:
+        return "割り切れる"
+    else:
+        return "割り切れない"
 
 result = check_len_list(ocr_list)
 
@@ -86,5 +100,32 @@ data = {'AVE': [], 'STD': [], 'RMS': []}
 for key, value in ocr_list:
     data[key].append(value)
 
-df = pd.DataFrame(data)
-df
+df_all = pd.DataFrame(data)
+df_all
+
+df1 = df_all.iloc[0::3, :]
+df11 = df1.rename(columns={'AVE': 'AVE1' , 'STD' : 'STD1' , 'RMS' : 'RMS1'})
+df2 = df_all.iloc[1::3, :]
+df22 = df2.rename(columns={'AVE': 'AVE2' , 'STD' : 'STD2' , 'RMS' : 'RMS2'})
+df3 = df_all.iloc[2::3, :]
+df33 = df3.rename(columns={'AVE': 'AVE3' , 'STD' : 'STD3' , 'RMS' : 'RMS3'})
+
+# 行インデックスをリセット
+df11_reset = df11.reset_index(drop=True)
+df22_reset = df22.reset_index(drop=True)
+df33_reset = df33.reset_index(drop=True)
+
+# リセットしたインデックスでデータフレームを結合
+result = pd.concat([df11_reset, df22_reset, df33_reset], axis = 1)
+
+result = result[['AVE1', 'AVE2', 'AVE3', 'STD1' , 'STD2' , 'STD3' , 'RMS1' , 'RMS2' , 'RMS3']]
+ave = (result['AVE1'] ** 2 + result['AVE2'] ** 2) ** 0.5
+
+std = (result['STD1'] ** 2 + result['STD2'] ** 2) ** 0.5
+rms = (result['RMS1'] ** 2 + result['RMS2'] ** 2) ** 0.5
+
+result1 = pd.concat([result, ave], axis = 1)
+
+print(result)
+
+result.to_csv(r"" + inputfile + r".csv")
